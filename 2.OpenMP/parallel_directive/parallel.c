d3232a1498e670233198f9487db56d2167120e6a
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2017-07-28 11:23:08 -0500

    Parallel directive

---
+++ 2.OpenMP/parallel_directive/parallel.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>     /* Standard Library: EXIT_SUCCESS */
+#include <stdio.h>      /* Standard I/O Library: printf */
+
+int main(void){
+
+    printf("Hello World !");
+
+    return EXIT_SUCCESS;
+};
