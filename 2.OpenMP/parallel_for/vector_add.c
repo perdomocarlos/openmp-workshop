ea28f35b0277f484e788e2ea145480636bdad8e9
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2017-07-31 09:03:10 -0500

    Parallel for directive

---
+++ 2.OpenMP/parallel_for/vector_add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>      /* Standard I/O Library: printf */
+#include <stdlib.h>     /* Standard Library: malloc, calloc, free, ralloc */
+#include <string.h>     /* String funtions: strcpy*/
+
+/* Array size */
+#define N 99000000
+
+/* Creating an alias for  unsigned long int datatype */
+typedef unsigned long int long_int;
+
+int main(){
+
+    /*  Memory initialization for arrays A, B, C */
+    long_int * A = (long_int *) malloc( N * sizeof(long_int) );
+    long_int * B = (long_int *) malloc( N * sizeof(long_int) );
+    long_int * C = (long_int *) malloc( N * sizeof(long_int) );
+
+    /* Arrays content initialization */
+    for(int i=0; i<N; ++i) A[i] = B[i] = C[i] = 1;
+
+    /* Add and multiplication C = A * B + A */
+    for(int j=0; j<N; ++j) C[j] = A[j] * B[j] + A[j] ;
+    
+    /* Answere validation */
+    char validation[10];
+    strcpy(validation,"Ok");
+    for(int k=0; k<N; ++k) 
+        if (C[k] != 2) strcpy(validation,"Fail");
+
+    printf ( "\t%d \t\t %s \t\t ", N, validation );
+
+    /* Releasing memory */
+    free(A);
+    free(B);
+    free(C);
+
+    return EXIT_SUCCESS;
+}
