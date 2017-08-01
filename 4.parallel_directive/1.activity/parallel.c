8376676458fbf5bc584d09dd765c1a2bf0075c61
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2017-08-01 11:58:56 -0500

    Runtime funtions activity

---
+++ 4.parallel_directive/1.activity/parallel.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>     /* Standard Library: EXIT_SUCCESS */
+#include <stdio.h>      /* Standard I/O Library: printf */
+// 1.Import the omp.h funtion library here
+
+
+int main(void){
+
+    // 2.Call the routine omp_set_num_therads here
+
+    // 3.Create a parallel región here
+    // 3.1.Get thread id (in the parallel región)
+    // 3.2 Print thread id (in the parallel región)
+
+    return EXIT_SUCCESS;
+};
